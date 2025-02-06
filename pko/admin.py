da974266d6482caa2a1c46d31b09a5597ace858a
matveyvarlamov <matvejvarlamov495@gmail.com>
2025-02-06 11:02:51 +0700

    Обновил админку

--- pko/admin.py
+++ pko/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from invoice.models import PkoDocument
 
-# Register your models here.
+admin.site.register(PkoDocument)
--- rko/admin.py
+++ rko/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from invoice.models import RkoDocument
 
-# Register your models here.
+admin.site.register(RkoDocument)
