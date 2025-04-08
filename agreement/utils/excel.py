7b0b070171daea9a24f908e65a75366fb525db2b
matveyvarlamov <matvejvarlamov495@gmail.com>
2025-04-08 11:33:35 +0700

    Test delete is_stamp

--- agreement/utils/excel.py
+++ agreement/utils/excel.py
@@ -6,19 +6,19 @@ import base64
 
 
 def create_agreement_excel(html, organization, is_stamp, download_pdf=False):
-    if organization and organization.stamp and is_stamp:
-        stamp_data = organization.stamp.read()
-        stamp_base64 = base64.b64encode(stamp_data).decode('utf-8')
-        stamp_html = f'<img src="data:image/png;base64,{stamp_base64}" style="height:128px; width:128px; object-fit:contain">'
-        html = html.replace('{print_stamp}', stamp_html)
-
-        sign_data = organization.signature.read()
-        sign_base64 = base64.b64encode(sign_data).decode('utf-8')
-        sign_html = f'<img src="data:image/png;base64,{sign_base64}" style="height:auto; width:80px; object-fit:contain">'
-        html = html.replace('{print_sign_director}', sign_html)
-    else:
-        html = html.replace('{print_stamp}', 'М.П.')
-        html = html.replace('{print_sign_director}', '')
+    # if organization and organization.stamp and is_stamp:
+    #     stamp_data = organization.stamp.read()
+    #     stamp_base64 = base64.b64encode(stamp_data).decode('utf-8')
+    #     stamp_html = f'<img src="data:image/png;base64,{stamp_base64}" style="height:128px; width:128px; object-fit:contain">'
+    #     html = html.replace('{print_stamp}', stamp_html)
+    #
+    #     sign_data = organization.signature.read()
+    #     sign_base64 = base64.b64encode(sign_data).decode('utf-8')
+    #     sign_html = f'<img src="data:image/png;base64,{sign_base64}" style="height:auto; width:80px; object-fit:contain">'
+    #     html = html.replace('{print_sign_director}', sign_html)
+    # else:
+    #     html = html.replace('{print_stamp}', 'М.П.')
+    #     html = html.replace('{print_sign_director}', '')
 
     convertapi.api_credentials = 'secret_K6wBgPb3icOrsjW5'
 
